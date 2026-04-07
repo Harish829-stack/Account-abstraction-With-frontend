e124ab74b768fd29ff9330e9e4cd0bf14ba8214b
Harish829-stack <harish@chicmicstudios.in>
2026-04-07 15:07:40 +0530

    verified working with uniswap v2 dex

--- frontend/src/utils/abis.js
+++ frontend/src/utils/abis.js
@@ -40,6 +40,7 @@ export const ERC20_ABI = [
   "function balanceOf(address) view returns (uint256)",
   "function allowance(address owner, address spender) view returns (uint256)",
   "function approve(address spender, uint256 amount) returns (bool)",
+  "function transfer(address to, uint256 amount) returns (bool)",
   "function transferFrom(address from, address to, uint256 amount) returns (bool)",
   "function symbol() view returns (string)",
   "function decimals() view returns (uint8)"
